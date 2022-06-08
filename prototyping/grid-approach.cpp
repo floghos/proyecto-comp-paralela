651e52b04dba42518b11324cb5a17d0b9272eb71
Pablo Furet <caleuche94@gmail.com>
2022-06-08 17:48:29 -0400

    a-star now working! input maps must follow testmap format

--- prototyping/grid-approach.cpp
+++ prototyping/grid-approach.cpp
@@ -22,13 +22,6 @@ bool operator==(const Coord& lhs, const Coord& rhs) {
 
 typedef pair<double, Coord> p_dc;
 
-void reconstruct_path(int current, vector<int> const &cameFrom) {
-    /*
-    COPIED FROM soa_approach
-    CHECK WHAT NEEDS TO BE REWORKED
-    */
-
-    vector<int> path;
 void reconstruct_path(Coord current, vector<vector<Coord>> const &cameFrom) {
     vector<Coord> path;
     path.push_back(current);
@@ -48,17 +41,19 @@ void reconstruct_path(Coord current, vector<vector<Coord>> const &cameFrom) {
 
 
 struct Cell {
-    // int x, y;
-    // I don't know if these'll be necessary, since coords can be calculated
+    // I don't know if Coords will be necessary, since they can be calculated
     // based on the index, but it might be faster to just save them.
-    Coord pos;
+    // Coord pos; // so far it has been unneeded
     bool wall = false;
     // bool goal = false;
-    int w = 0;
+    int w = 1;
     // w stands for weight
     // The idea for this is that walking through this cell has a greater cost
     // in other words, I... LOVE... YOU... *cough* excuse me...
     // in other words, the cost of every incoming edge needs to add w.
+
+    // Seriously considering ditching this struct and make the graph just a
+    // matrix of chars
 };
 
 
@@ -75,14 +70,23 @@ public:
     // int GOAL_Y_COR;
 
     // Graph Constructor
-    Graph(vector<vector<int>> map) {
+    Graph(vector<vector<char>> const &map) {
         // resize the vector to hold `n` elements of type `vector<int>`
-        // n = map.size();
-        // m = map[0].size();
-        //
-        // grid.resize(map.size());
-        // add edges to the directed graph
+        n = map.size();
+        m = map[0].size();
+        grid.resize(n, vector<Cell> (m));
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == '.') {
+                    grid[i][j].wall = false;
+                }
+                if (map[i][j] == '#') {
+                    grid[i][j].wall = true;
+                }
+                // grid[i][j] = map[i][j];
+            }
+        }
     }
 
     void setGoal(Coord g) {
@@ -99,9 +103,9 @@ public:
                 _y = node.y + j;
 
                 //check if we are off limits
-                if (_x < 0 || _x > n) continue;
-                if (_y < 0 || _y > m) continue;
-                //also avoid self
+                if (_x < 0 || _x >= n) continue;
+                if (_y < 0 || _y >= m) continue;
+                //and also avoid self
                 if (i == 0 && j == 0) continue;
 
                 if (grid[_x][_y].wall == false) {
@@ -114,6 +118,7 @@ public:
     }
 
     double h(Coord cell) {
+        cerr << "calculating H of " << cell.x << "," << cell.y << endl;
         return sqrt((pow((cell.x - goal.x), 2) + pow((cell.y - goal.y), 2)));
     }
 
@@ -133,6 +138,11 @@ public:
 typedef pair<double, pair<int,int> > pdp;
 
 bool is_in_pq(Coord v, priority_queue<pdp, vector<pdp>, greater<pdp>> pq) {
+    /*
+    Checks if a given Coord element is present in the given PQ
+    It recieves the PQ as a copy by value and empties it, checking one
+    element at a time
+    */
     while (!pq.empty()) {
         pair<int, int> t = pq.top().second;
         Coord check = {t.first, t.second};
@@ -148,14 +158,21 @@ void a_star_search(Coord _start, Coord _goal, Graph graph) {
     if (graph.n == 0 || graph.m == 0) return;
     // priority_queue<p_dc, vector<p_dc>, greater<p_dc> > openSet;
     // replaced the pair<double, coord> PQ for this one cuz I couldn't make the former work
+    cerr << "creating PQ" << endl;
     priority_queue<pdp, vector<pdp>, greater<pdp>> openSet;
 
     //inserting first element to the PQ (starting node)
+    cerr << "pushing starting point in PQ" << endl;
     pair<int, int> s = make_pair(_start.x, _start.y);
     openSet.push(make_pair(0.f, s));
 
     //map used to reconstruct path after reaching goal.
+    cerr << "allocating memory for auxiliary vectors" << endl;
+
     vector<vector<Coord>> cameFrom(graph.n, vector<Coord> (graph.m, {-1, -1}));
+    // We'll say that the starting cell's parent is itself
+    // This fact is used in the recreation of the path to stop the loop.
+    cameFrom[_start.x][_start.y] = _start;
 
     //need to initialize these to max double
     vector<vector<double>> gScore(graph.n, vector<double> (graph.m, DBL_MAX));
@@ -163,11 +180,19 @@ void a_star_search(Coord _start, Coord _goal, Graph graph) {
     vector<vector<double>> fScore(graph.n, vector<double> (graph.m, DBL_MAX));
     fScore[_start.x][_start.y] = graph.h(_start);
 
-    while (!openSet.empty()) {
+    // just testing!!! this counter will limit the number of iterations to prevent endless loops
+    int forceQuit = 0;
+
+    while (!openSet.empty() && forceQuit < 1000) {
+        forceQuit++;
+
         pair<int, int> aux = openSet.top().second;
         Coord current = {aux.first, aux.second};
+        openSet.pop();
+        cerr << aux.first << "," << aux.second << endl;
         if (current == graph.goal) {
-            // reconstruct_path(current);
+            cerr << "goal reached, recreating path" << endl;
+            reconstruct_path(current, cameFrom);
             return;
         }
 
@@ -185,35 +210,38 @@ void a_star_search(Coord _start, Coord _goal, Graph graph) {
             }
         }
     }
+    cout << "No path found" << endl;
 }
 
 
 int main(int argc, char const *argv[]) {
-    /* read map from file*/
     int n, m;
-    vector<vector<int>> map;
-    cerr << "check map vector " << map.size() << endl;
+    cin >> n >> m;
 
-
-    Coord a, b;
-
-    n = 3;
-    m = 5;
-
-    // this was just for testing
-    // vector<vector<double>> gScore(n, vector<double> (m, DBL_MAX));
-    // cout << "gScore dims: " << gScore.size() << "x" << gScore[0].size() << endl;
-
-    // reading from std input
-    // cin >> n >> m;
+    vector<vector<char>> map(n, vector<char> (m, '.'));
+    Coord s, g;
+    cerr << "reading input" << endl;
+    /* read map from std input*/
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            /*TODO: read from input amd save to map*/
-            continue;
+            cin >> map[i][j];
+            if (map[i][j] == 's') {
+                s = {i, j};
+                map[i][j] == '.';
+            }
+            if (map[i][j] == 'g') {
+                g = {i, j};
+                map[i][j] == '.';
+            }
         }
     }
+
+    cerr << "creating graph" << endl;
     Graph graph(map);
-    a_star_search(a, b, graph);
+    graph.setGoal(g);
+
+    cerr << "running a_star" << endl;
+    a_star_search(s, g, graph);
 
     return 0;
 }
