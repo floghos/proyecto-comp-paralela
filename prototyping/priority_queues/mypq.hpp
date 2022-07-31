ece06b909217cd5c617cd2bf1c9df934bb322976
Pablo <caleuche94@gmail.com>
2022-07-31 00:22:05 -0400

    MyPQ now fully functional

--- prototyping/priority_queues/mypq.hpp
+++ prototyping/priority_queues/mypq.hpp
@@ -8,22 +8,14 @@
 #define MYPQ_H
 
 
-// using namespace std;
-
-// typedef pair<double, pair<int,int> > pdp; // pair double-pair
-// typedef pair<double, int> pdi; // pair double-int
-
-// template<class K, class V> class MyPQ;  // pre-declare the template class itself
-// template<class K, class V>bool operator < (const std::pair<K, V>& lhs, const std::pair<K, V>& rhs);
-
-// bool operator < (const std::pair<int, char>& lhs, const std::pair<int, char>& rhs) {
-//     return true;
-// }
-
 template <class K, class V>
 class MyPQ {
-    /* This is a min-heap.
+    /* This is a min-heap, <key, value> based priority queue.
      * It sorts items putting first the one with the lowest "key".
+     * I chose to use pairs of template classes instead of just one template class that allows for pairs.
+     * This is so I can then look for an element using only the "value", since for my purposes this "value" is the ID of the element,
+     * while the "key" is just the priority around which the element is sorted.
+     * Maybe I should rename the template classes?
      */
 
     /*
@@ -39,22 +31,21 @@ class MyPQ {
 
 private:
     std::vector<std::pair<K, V>> mArray;
-    std::map<std::pair<K, V>, unsigned int> mMap;
+    std::map<V, unsigned int> mMap;
 
-    void swap(unsigned int a, unsigned int b) {
-        std::pair<K, V> aux = mArray[a];
-        mArray[a] = mArray[b];
-        mArray[b] = aux;
+    void swap(unsigned int idx_a, unsigned int idx_b) {
+        std::pair<K, V> aux = mArray[idx_a];
+        mArray[idx_a] = mArray[idx_b];
+        mArray[idx_b] = aux;
 
-        mMap[mArray[a]] = a;
-        mMap[mArray[b]] = b;
+        mMap[mArray[idx_a].second] = idx_a;
+        mMap[mArray[idx_b].second] = idx_b;
     }
 
 public:
 
     MyPQ() {
         mArray.push_back(std::pair<K, V>());
-//         mSize = 0;
     }
 
     std::pair<K, V> top() {
@@ -67,10 +58,10 @@ public:
 
         std::pair<K, V> newEntry = std::make_pair(key, id);
         mArray.push_back(newEntry);
-        mMap[newEntry] = n;
+        mMap[id] = n;
         unsigned int i = n;
         while (i > 1) {
-            if (mArray[i] < mArray[i/2]) {
+            if (mArray[i].first < mArray[i/2].first) {
                 swap(i, i/2);
                 i = i/2;
             } else {
@@ -78,32 +69,32 @@ public:
             }
         }
     }
+    void push(std::pair<K, V> p) {
+        push(p.first, p.second);
+    }
 
-    void pop() {
-        /*
-         *TO DO :
-         * Need to fix behaviour when popping elements on an empty PQ.
-         * i.e. when mArray.size()-1 = 0
-         */
-//         std::pair<K, V> ans = mArray[1];
+    std::pair<K, V> pop() {
+        if (size() == 0) return std::pair<K, V>();
+
+        std::pair<K, V> ans = mArray[1];
         unsigned int n = mArray.size()-1;
 
-        mMap.erase(mArray[1]);
+        mMap.erase(mArray[1].second);
         mArray[1] = mArray[n];
         mArray.pop_back();
-        mMap[mArray[1]] = 1;
+        mMap[mArray[1].second] = 1;
         n--;
         unsigned int i = 1;
         while (2*i <= n) {
-            if ((2*i+1 > n) || (mArray[2*i] < mArray[2*i+1])) {
-                if (mArray[i] > mArray[2*i]) {
+            if ((2*i+1 > n) || (mArray[2*i].first < mArray[2*i+1].first)) {
+                if (mArray[i].first > mArray[2*i].first) {
                     swap(i, 2*i);
                     i = 2*i;
                 } else {
                     break;
                 }
             } else {
-                if (mArray[i] > mArray[2*i+1]) {
+                if (mArray[i].first > mArray[2*i+1].first) {
                     swap(i, 2*i+1);
                     i = 2*i+1;
                 } else {
@@ -111,19 +102,47 @@ public:
                 }
             }
         }
-//         return ans;
+        return ans;
     }
 
     void decreaseKey(V id, K newKey) {
-        /* ... */
+        /* This will only allow for keys to be decreased, taking the element up the heap.
+         * It should be easy enough to generalize this function into going up or down
+         */
+        unsigned int i = mMap[id];
+        if (mArray[i].first > newKey) {
+            mArray[i].first = newKey;
+            while (i > 1) {
+                if (mArray[i].first < mArray[i/2].first) {
+                    swap(i, i/2);
+                    i = i/2;
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
     int size() {
+        /* It's size()-1 because I am sacrificing mArray[0] to be a useless position,
+         * just to make the rest of the work with indices look a bit cleaner
+         */
         return mArray.size()-1;
     }
 
+    bool is_in_pq(V id) {
+        return mMap.find(id) != mMap.end();
+    }
+
     bool empty() {
-        return bool();
+        return size() <= 0;
+    }
+
+    void show_queue() {
+        for (int i = 0; i < mArray.size(); i++) {
+            std::cout << i << "(" << mArray[i].first << "," << mArray[i].second << ")" << " ";
+        }
+        std::cout << std::endl;
     }
 };
 
