007ed1027716cf40c752c772714cd7a184514df2
Pablo <caleuche94@gmail.com>
2022-08-01 02:31:02 -0400

    all parallelpq methods implemented. Only hash tested to be working correctly so far

--- prototyping/priority_queues/parallelpq.cpp
+++ prototyping/priority_queues/parallelpq.cpp
@@ -1,38 +1,80 @@
 #include "parallelpq.h"
 
+unsigned int ParallelPQ :: hash(int id) {
 
-typedef pair<double, int> pdi; // pair double-int
+    uint64_t h = murmur64((uint64_t) id);
+    uint64_t mask = (1 << mBits) - 1;
+    unsigned int idx = (unsigned int) h & mask;
+    return (unsigned int)(idx);
+}
 
+ParallelPQ :: ParallelPQ(int bits) {
+    mBits = bits;
+    unsigned int arraySize = 1 << (bits);
+    for (unsigned int i = 0; i < arraySize; i++) {
+        mPQArray.push_back(priority_queue<pdi, vector<pdi>, greater<pdi>>());
+    }
+    lock = (omp_lock_t *) malloc(arraySize*sizeof(omp_lock_t));
 
-ParallelPQ :: ParallelPQ(int level) {
+    for (unsigned int i = 0; i < arraySize; i++) {
+        omp_init_lock(&(lock[i]));
+    }
 
 }
 
-Coord ParallelPQ :: top() {
-    return Coord();
+void ParallelPQ :: push(double fScore, unsigned int cell_id) {
+    unsigned int idx = hash(cell_id);
+
+    omp_set_lock(&(lock[idx]));
+    //record lowest previous to the insert
+    pdi oldLowest = mPQArray[idx].top();
+    //insert new element
+    mPQArray[idx].push(pdi{fScore, cell_id});
+    //check new lowest element
+    pdi newLowest = mPQArray[idx].top();
+    // If the lowest element in the PQ hasn't changed, we are done.
+    // Otherwise, we need to update the key of the corresponding PQ in the
+    // manaager PQ
+    if (oldLowest.second != newLowest.second) {
+        mManager.decreaseKey(idx, newLowest.first);
+    }
+    omp_unset_lock(&(lock[idx]));
+
 }
 
-void ParallelPQ :: push() {
-
+std::pair<double, int> ParallelPQ :: top() {
+    // ask the manager which PQ has the lowest item WITHOUT removing it
+    unsigned int idx = mManager.top().second;
+    // Take a peek of said item from the corresponding PQ
+    pdi answer = mPQArray[idx].top();
+    return answer;
 }
 
 void ParallelPQ :: pop() {
-    int first = mManager.top().second;
+    // ask the manager which PQ has the lowest item and remove it
+    unsigned int idx = mManager.top().second;
+    // EXTRACT said item from the corresponding PQ
+    mPQArray[idx].pop();
+    // Give manager new lowest item of the current PQ
+    auto temp = mPQArray[idx].top();
+    mManager.push(temp.first, idx);
+}
 
-
+double find(unsigned int cell_id) {
+    return double(-1);
 }
 
 int ParallelPQ :: size() {
     int count = 0;
-    for (int i = 0; i < mPQ_array.size(); i++) {
-        count += mPQ_array[i].size();
+    for (int i = 0; i < mPQArray.size(); i++) {
+        count += mPQArray[i].size();
     }
     return int();
 }
 
 bool ParallelPQ :: empty() {
-    for (int i = 0; i < mPQ_array.size(); i++) {
-        if (!mPQ_array[i].empty()) return false;
+    for (int i = 0; i < mPQArray.size(); i++) {
+        if (!mPQArray[i].empty()) return false;
     }
     return true;
 }
--- prototyping/priority_queues/parallelpq.h
+++ prototyping/priority_queues/parallelpq.h
@@ -1,30 +1,41 @@
 #include <queue>
 #include <vector>
 #include <map>
+#include "murmurhash.hpp"
+#include "mypq.hpp"
+
 
 #ifndef PARALLELPQ_H
 #define PARALLELPQ_H
 
-struct Coord {
-    int x, y;
-};
 using namespace std;
 
-typedef pair<double, pair<int,int> > pdp; // pair double-pair
-typedef pair<double, int> pdi; // pair double-int
+ #include <omp.h>
+
+typedef std::pair<double, int> pdi; // pair double-pair
+// typedef pair<double, int> pdi; // pair double-int
 
 class ParallelPQ {
 private:
-    priority_queue<pdi, vector<pdi>, greater<pdi>> mManager;
-    vector<priority_queue<pdp, vector<pdp>, greater<pdp>>> mPQ_array;
-    map<Coord, int> mMap;
+// Members
+    // bits determines the size of the mPQArray, as in mPQArray.size() = 2^mBits,
+    // indexes of the array will go from 0 to 2^mBits-1
+    unsigned int mBits;
+    MyPQ<double, unsigned int> mManager;
+    omp_lock_t *lock;
+    vector<priority_queue<pdi, vector<pdi>, greater<pdi>>> mPQArray;
+    unsigned int hash(int id);
+//     map<unsigned int, double> mMap; //keeps track which cells have entered the PQ and with which fScores.
+
+// Methods
 
 public:
-
-    ParallelPQ(int level);
-    Coord top();
-    void push();
+// Methods
+    ParallelPQ(int bits);
+    std::pair<double, int> top();
+    void push(double fScore, unsigned int cell_id);
     void pop();
+    double find(unsigned int cell_id);
     int size();
     bool empty();
 };
